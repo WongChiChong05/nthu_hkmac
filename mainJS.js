64f636db1e181a84d9b50a84515dc63f46a623e8
WongChiChong05 <wongchichong9@gmail.com>
2024-08-27 01:41:02 +0800

    Add files via upload

---
+++ mainJS.js
@@ -0,0 +1,289 @@
+const personalbg = [
+    'url(./member_photo/photo1.jpg)',
+    'url(./member_photo/photo2.jpg)',
+    'url(./member_photo/photo3.jpg)',
+    'url(./member_photo/photo4.jpg)',
+    'url(./member_photo/photo5.jpg)',
+    'url(./member_photo/photo6.jpg)',
+    'url(./member_photo/photo7.jpg)',
+    'url(./member_photo/photo8.jpg)',
+    'url(./member_photo/photo9.jpg)',
+    'url(./member_photo/photo10.jpg)'
+]
+
+const boxbg = [
+    'unity',
+    'support',
+    'culture',
+    'voice',
+    'career'
+]
+
+const memberIntro = [
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>Hello大家好😬我係113學年嘅港澳會會長陸梓鳴，系級係電機系27級，大家可以叫我Jimmy。平時鍾意打籃球、篤波、探險、上山下海、夜衝、喺大草坪發吽哣……好多好多，總之除咗讀書嘅嘢大家都可以約我出黎一齊做😈btw篤波歡迎單挑，贏到我嘅話請你食金展（啲餸我揀）
+        <br><br>
+        無論你係新生舊生、咩年級都好，港澳會都會比你家鄉嘅歸屬感，所以都好歡迎大家join我地啲活動一齊開心一齊玩🔥
+        <br><br>
+        希望我哋今年可以傳承到上年啲傳統，做好新生嘅交接工作，繼續搞好港澳週國際週啲對外活動。同時都整返個港澳會旅行🏖️讀書咁攰，放假梗係要一齊玩一齊開心一齊chill下🤩總之呢度規矩就係安全第一，開心第二，其他嘢第三😎
+        <br><br>
+        最後想對新生們講，如果你地唔慣呢度嘅生活、學習模式、同同學嘅相處方式又或者係對大學生活充滿恐懼和迷惘……之類之類，歡迎揾我傾，一路玩一路傾。講出嚟一定會舒服啲，我都做過新生，我明係咩feel，幫得到嘅話一定會幫你！</p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>大家好^_^我係資工系大二嘅黃子聰，嚟緊呢一年將會係港澳會嘅副會長，請大家多多指教～
+        平時鍾意睇吓動漫、聽吓band同埋玩各種運動（大多數常見嘅運動種類你講得出我應該都識少少，所以平時想打吓波做吓運動都可以搵下我）。
+        如果大家遇到任何問題都可以問我，我都會盡力解答大家嘅疑問，同埋幫助大家適應依個新嘅環境。希望可以幫助到大家有更加理想嘅大學生活～</p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>
+        hello大家好呀~我係第十屆國立清華大學港澳會副會長_馮耕勤！！！讀緊工工系year 3，大家可以叫我做阿Fred 黎緊多多指教！
+        <br><br>就嚟開學啦~離鄉別井去讀書一定係落咗好大決心犧牲好多嘢先嚟到呢度。台灣讀書對大家係一個新嘅開始，要面對嘅都唔一樣。各位咁遠水路嚟到台灣讀書都辛苦啦
+        <br><br>無論你哋係大學、碩士或博士都好，
+        <br>喺港澳會裏面大家可以一齊組團去台灣各地挽出遊~
+        <br>仲可以識到好多新朋友同學長姐
+        <br>台灣讀書對大家係一個新嘅開始，要面對嘅都唔一樣。
+        <br>不同地方
+        <br>不同語言
+        <br>不同人物
+        <br><br>我希望呢啲不同，可以同大家一齊面對。
+        <br>港澳人幫港澳人
+        <br>無論你係香港定係澳門大家平時講開嘅都係廣東話
+        <br>所以我哋大家都係一條船上面
+        <br>台語國語
+        <br>我哋唔係最擅長亦都唔熟悉。
+        <br>人在客鄉身不由己
+        <br>希望大家可以記住自己嘅身份求同存異。
+        <br>雖然我嘅身份唔同咗，但係我相信我哋嘅目標都係一樣。
+        <br>多謝你哋睇到呢度
+        <br>希望快啲見到大家！</p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h1> self-intro </h1> <br>
+        <p>
+       大家好，我係讀緊資工系大三嘅何子揚，可以叫我Marcus，最近開始嘗試攝影，平時會去踩單車，嚟緊嘅呢一年我會繼續擔任港澳會嘅財務一職！
+歡迎各位新生加入我哋！</p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2><br>
+        <p>
+       大家好～我係藝設系設計組大三嘅陳錦而，可以叫我Zoe✌️今年擔任港澳會嘅美宣，大家喺課業上有咩問題都可以問我🙂‍↕️有玩嘅都記得可以叫埋我！希望之後會有好多機會見到大家，chur起🙌</p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2><br>
+        <p>
+        hi大家好，我係林成宇，平時叫我成宇or阿Ling就得。目前讀緊動力機械工程學系大三，係未來一年擔任港澳會總務裡個職位。
+        <br><br>
+大家飄洋過海黎到台灣讀書有任何問題都可以諮詢我哋，包括但不限於學業 日常生活等。另外如果你哋鐘意爬山 騎車 等一系列小癲嘅活動都可以搵我！
+        <br><br>
+最後希望大家無論係懷著期待定係任何心情踏上前往台灣嘅飛機，係落地後大家都可以好好的感受裡個寶島 裡個清華大工地帶俾你哋酸甜苦辣，希望各位即將黎到台灣嘅新同學可以度過一個充實且難忘嘅四年。
+        </p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>
+        大家好～
+        <br>我係幼教26孫曉琪，係今年港澳會既總務，大家可以叫我Kiki就得😊
+        <br>歡迎大家加入港澳會並參加活動！！！
+        <br>希望新生既大家可以一齊享受台灣大學生活
+        </p>
+    </div>
+    `,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>
+        早晨！我係動機大三嘅吳俊希，大家叫我Andy得㗎啦~
+        <br>
+        今次係我第二年任職港澳會幹部，從副會長「燉冬菇」到公關。之前我係負責對內嘅聯繫，依家我就嚟負責對外嘅工作。我會盡力幫助大家更快融入校園生活，如果大家遇到日常瑣碎的事情我都會盡力幫你哋解決，期待與你哋相見！
+        </p>
+    </div>`,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2> <br>
+        <p>
+        🗣️Yo!!!! 大家好~我係呢屆港澳會嘅社長大人
+        <br>講笑～其實我係負責做文案管理😆
+        <br>藝設系大三嘅梁靜雯，大家叫我石零就好(*¯︶¯*)
+        <br>.
+        <br>唔理你係新生！舊生！又或者係大前輩！
+        <br>都希望大家踴躍參加我地港澳會嘅活動✨
+        <br>Love & Peace☺️我地會盡力而為去搞好啲活動✌🏻✌🏻✌🏻
+        <br>.
+        <br>一期一會🍀
+        <br>希望係各位嘅台灣留學之旅，我地可以一齊創造好多美好嘅回憶，享受當下嘅青春！
+        </p>
+    </div>`,
+    `
+    <div class = introContent style = "margin: 20px 15px 20px; ">
+        <h2> self-intro </h2><br>
+        <p>
+       Hello大家好～我叫黎嘉希 可以叫我fairy🤣 我而家係教育學院學士班大二~ 並且喺未來呢一年擔任港澳會嘅策劃部~希望大家有一個愉快嘅學習旅程👀~得閒飲茶🍵想同我傾計我都好樂意㗎！🤣
+        </p>
+    </div>`
+    
+
+]
+const showBoxSize = [
+    '300px',
+    '260px',
+    '140px'
+
+]
+
+let largeMedia = window.matchMedia("(max-width: 1200px) and (min-width: 990px)");
+let mediumMedia = window.matchMedia("(max-width: 990px) and (min-width: 750px)");
+let smallestMedia = window.matchMedia("(max-width: 750px)");
+let photo = document.querySelector('.photoList .photo img');
+const leftBtn =  document.querySelector('.left');
+const rightBtn =  document.querySelector('.right');
+const btnImg =  document.querySelectorAll('.peopleImg');
+const member =  document.querySelector('.member');
+const box = document.querySelectorAll('.box');
+const pur = document.querySelector('.purpose');
+let flag = 0;
+
+for (let i = 0; i <= 4; i++){
+    box[i].style.background = `url(./purpose/${boxbg[i]}.png) no-repeat`;
+    box[i].style.backgroundPositionX = 'center';
+    box[i].style.backgroundSize = '100%';
+}
+
+for (let i = 0; i <= 9; i++){
+    btnImg[i].style.background = `url(./member_photo/icon${i + 1}.jpg) no-repeat` ;
+    btnImg[i].style.backgroundPositionX = 'center';
+    btnImg[i].style.backgroundSize = '100%';
+}
+
+pur.addEventListener('click', function(e){
+    if (e.target.className === 'box'){
+        const show = document.querySelectorAll('.showbox');
+        const leave = document.querySelectorAll('.showbox .crossoutBtn');
+        const context = document.querySelectorAll('.showbox .purposeText');
+        const i = e.target.dataset.id - 1
+        show[i].style.top = 0;
+        let size = 0;
+        if (largeMedia.matches || mediumMedia.matches){
+            size = 1;
+        }
+        else if (smallestMedia.matches){
+            size = 2;
+        }
+        else{
+            size = 0;
+        }
+        console.log(size);
+        show[i].style.height = showBoxSize[size];
+        if (size === 2){
+            show[i].style.overflowY = 'scroll';
+        }
+        else{
+            show[i].style.overflowY = i === leave.length - 1 ? 'scroll' : 'hidden';
+        }
+        leave[i].style.display = context[i].style.display = 'block';
+        leave[i].addEventListener('click', function(){
+            show[i].style.height = 0;
+            show[i].style.top = showBoxSize[size];
+            leave[i].style.display = context[i].style.display = 'none';
+        })
+        
+    }
+})
+
+member.addEventListener('click', function(e){
+    if (e.target.className === 'card'){
+        console.log(e)
+        const card = document.querySelector('.card');
+        card.classList.add('information-card');
+        card.style.display = 'block';
+        let leftElement = document.createElement('div');
+        leftElement.classList.add('cardLeft');
+        leftElement.style.background = personalbg[e.target.dataset.id - 1];
+        leftElement.style.backgroundSize = '100%';
+        console.log(card.dataset.id - 1);
+        console.log(leftElement.style);
+        let rightElement = document.createElement('div');
+        rightElement.classList.add('cardRight');
+        rightElement.innerHTML = memberIntro[e.target.dataset.id - 1];
+        let leaveBtn = document.createElement('div');
+        leaveBtn.classList.add('crossoutBtn');
+        leaveBtn.innerHTML = 'X';
+        rightElement.appendChild(leaveBtn);
+        card.appendChild(leftElement);
+        card.appendChild(rightElement);
+        for (let i = 0; i <= 9; i++){
+            btnImg[i].disabled = true;
+        }
+        let leave =  document.querySelector('.cardRight .crossoutBtn');
+        leave.addEventListener('click', function(){
+            leftElement.style.background = 'none';
+            rightElement.removeChild(leaveBtn);
+            card.removeChild(leftElement);
+            card.removeChild(rightElement);
+            card.classList.remove('information-card');
+            for (let i = 0; i <= 9; i++){
+                btnImg[i].disabled = false;
+            }
+        })
+    }     
+})
+
+
+let countImg = 1;
+console.log(photo);
+
+function showPhoto(){
+    console.log(countImg);
+    photo.src = `./activity_photo/nthu_activity_${countImg}.jpg`;
+
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector(`.changePoint li:nth-child(${countImg})`).classList.add('active');
+}
+
+const photoTime = setInterval(function(){
+    countImg++;
+    console.log(photo.style.left);
+    if (countImg >= 6){
+        countImg = 1;
+    }
+    showPhoto();
+    
+},5000);
+
+leftBtn.addEventListener('click',function(){
+    countImg--;
+    if (countImg <= 0){
+        countImg = 5;
+    }
+    showPhoto();
+    photo.style.left = '0px';
+});
+
+rightBtn.addEventListener('click',function(){
+    countImg++;
+    
+    if (countImg >= 6){
+        countImg = 1;
+    }
+    showPhoto();
+    photo.style.left = '0px';
+})
+
+
+
+
